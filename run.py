e5afb5eebcc6fe0fd937198d8c852c2986350fdf
slava_kyiv <vyacheslavkondriyanenko@gmail.com>
2023-01-19 12:45:26 +0000

    Create function to get list ingredients

--- run.py
+++ run.py
@@ -11,3 +11,16 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('coffee_machine')
+
+
+def get_drink_ingredients(index):
+    """
+    Gets user input and returns ingredients integer list
+    """
+    drink_column = SHEET.worksheet("menu").col_values(int(index))[1:4]
+    ingredients = [int(value) for value in drink_column]
+    return ingredients
+
+
+ingredients = get_drink_ingredients(1)
+print(ingredients)
